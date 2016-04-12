a40f0d4118e57323672359a7f40ba7459b0b6b35
SuongTran0911 <1312498@student.hcmus.edu.vn>
2016-04-12 08:14:26 +0700

    xong

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-var mongoose = require('mongoose');
 var express = require('express');
-mongoose.connect('mongodb://localhost', function(err){
-	if(err) throw err
-		console.log('connected!');
-	var app = express();
-	app.listen(3000, function(){
-		console.log('now listening on http://localhost:3000');
-	})
+var app = express();
+var ECT = require('ect');
+
+
+var ectRenderer = ECT({ watch: true, root: __dirname + '/views', ext : '.ect' });
+var routes = require('./routes');
+app.set('view engine', 'ect');
+app.engine('ect', ectRenderer.render);
+
+    app.use('/', routes);
+
+app.listen(3000, function(){
+console.log('Listening on port 3000');
 })
